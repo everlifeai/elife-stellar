92b7366d7c952cb0f2600c9811b0de5294900e57
Charles L <charles.lobo@gmail.com>
2021-05-05 22:58:16 +1000

    feat: use @elife/secret

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const cote = require('cote')({statusLogsEnabled:false})
 
 const u = require('@elife/utils')
 const luminate = require('@tpp/luminate')
+const secret_ = require('@elife/secret')
 
 const pwc = require('./pwc')
 
 const EVER_ISSUER = process.env.EVER_ISSUER || 'GDRCJ5OJTTIL4VUQZ52PCZYAUINEH2CUSP5NC2R6D6WQ47JBLG6DF5TE'
 
-const ethers = require("ethers")
 
 /*    understand/
  * Microservice key (identity of the microservice)
@@ -85,7 +85,7 @@ function loadWallet(msinfo, cb) {
 }
 
 function loadSecretWallet(msinfo, cb) {
-    loadSecretData((err, data) => {
+    secret_.loadSecretData((err, data) => {
         if(err) return cb(err)
         if(data.stellar && data.stellar.publicKey && data.stellar.secretKey) {
           msinfo.acc = {
@@ -99,99 +99,16 @@ function loadSecretWallet(msinfo, cb) {
     })
 }
 
-/*      way/
- * read the secret file and - ignoring comments - parse it
- * as a JSON and return the data
- */
-function loadSecretData(cb) {
-    fs.readFile(u.secretFile(), 'utf8', (err, data) => {
-      if(err) return cb(err)
-      try {
-        data = data.replace(/\s*#[^\n]*/g, "")
-        cb(null, JSON.parse(data))
-      } catch(e) {
-        cb(e)
-      }
-    })
-}
-
 function loadLuminateWallet(msinfo, cb) {
     loadPw(msinfo, (err) => {
         if(err) cb(err)
         else loadAccount(msinfo, err=>{
             if(err) cb(err)
-            else saveIntoSecretFile(msinfo.acc.secret, cb)
+            else secret_.updateWalletKey(msinfo.acc.secret, cb)
         })
     })
 }
 main()
-
-/*      outcome/
- * save the stellar (and eth) keys into the secret file
- * so we can load it from there the next time (migrate out of luminate
- * wallet)
- */
-function saveIntoSecretFile(secret, cb) {
-    let kp
-    try {
-        kp = StellarSdk.Keypair.fromSecret(secret)
-    } catch(e) {
-        return cb(`Invalid Stellar Secret Key: ${secret}`)
-    }
-
-    loadSecretData((err, data) => {
-        if(err) return cb(err)
-
-        if(!data.eth) {
-            const ewallet = ethers.Wallet.createRandom()
-            data.eth = {
-                address: ewallet.address,
-                publicKey: ewallet.publicKey,
-                privateKey: ewallet.privateKey
-            }
-        }
-
-        const old = []
-        if(data.stellar) {
-            old.push({
-                publicKey: data.stellar.publicKey,
-                secretKey: data.stellar.secretKey,
-            })
-            if(data.stellar.old) {
-                old.push(...data.stellar.old)
-            }
-        }
-        data.stellar = {
-            publicKey: kp.publicKey(),
-            secretKey: kp.secretKey(),
-        }
-        if(old.length) data.stellar.old = old
-
-        const lines = `# These are your SECRET keys.",
-#
-# Any one who has access to these keys has access to
-# your avatar and wallets and can use it to steal from
-# you and destroy your identity.
-#
-# NEVER show this to anyone!!!
-
-${JSON.stringify(data, null, 2)}
-
-# WARNING! It's vital that you DO NOT edit OR share your SECRET keys.
-# You can safely share your public name or any of the other public keys.
-# your public name: ${existingKeys.id}`
-
-
-        fs.chmod(u.secretFile(), 0o600, err => {
-            if(err) return cb(err)
-            fs.writeFile(u.secretFile(), lines, err => {
-                if(err) return cb(err)
-                fs.chmod(u.secretFile(), 0x100, cb)
-            })
-        })
-
-    })
-}
 
 /*      outcome/
  * Load the password from an encrypted file
@@ -309,7 +226,7 @@ function startMicroservice(msinfo) {
 
 function importNewWallet(msinfo, req, cb) {
     if(!req.secret) return cb('No Stellar Secret Key found to import!')
-    saveIntoSecretFile(req.secret, cb)
+    secret_.updateWalletKey(req.secret, cb)
 }
 
 function setNewPw(msinfo, req, cb) {
